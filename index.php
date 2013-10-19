0c213d464753e89ca65344c2a935770eeb053a2e
Olav Schettler <olav.schettler@cocomore.com>
2013-10-20 00:45:07 +0200

    Added Evernote PHP-SDK

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 require_once 'vendor/autoload.php';
 
 config('dispatch.views', './views');
+config('source', 'settings.ini');
 
 on('GET', '/', function () {
   echo "Hallo Welt";
 });
+
+dispatch();
